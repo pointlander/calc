1875cd781f12a025360e4afdc678c35a6a190dfd
Andrew Snodgrass <pointlander@gmail.com>
2026-07-30 03:27:47 -0500

    Use custom prompt

--- cmd/calc/main.go
+++ cmd/calc/main.go
@@ -5,34 +5,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-
-	"github.com/c-bata/go-prompt"
+	"os"
+	"strings"
 
 	"github.com/pointlander/calc"
 )
 
-func completer(d prompt.Document) []prompt.Suggest {
-	s := []prompt.Suggest{
-		{Text: "exp", Description: "The natural number raised to a value"},
-		{Text: "e", Description: "The natural number"},
-		{Text: "pi", Description: "The constant PI"},
-		{Text: "prec", Description: "Sets the precision for calculations"},
-		{Text: "simplify", Description: "Simplifies the expression"},
-		{Text: "derivative", Description: "Computes the symbolic derivative of the expression"},
-		{Text: "log", Description: "The natural logarithm of the input"},
-		{Text: "sqrt", Description: "The square root of the value"},
-		{Text: "cos", Description: "The cosine of the value"},
-		{Text: "sin", Description: "The sine of the value"},
-		{Text: "tan", Description: "The tangent of the value"},
-		{Text: "exit", Description: "Exit the application"},
-	}
-	return prompt.FilterHasPrefix(s, d.GetWordBeforeCursor(), true)
-}
-
 func main() {
+	scanner := bufio.NewScanner(os.Stdin)
 	for {
-		value := prompt.Input("> ", completer)
+		fmt.Print("> ")
+
+		if !scanner.Scan() {
+			break
+		}
+		value := strings.TrimSpace(scanner.Text())
 		if value == "exit" {
 			return
 		}
--- go.mod
+++ go.mod
@@ -4,19 +4,8 @@ go 1.25
 
 replace github.com/ALTree/bigfloat => github.com/pointlander/bigfloat v0.0.0-20201211040007-2034219fcdd4
 
-require (
-	github.com/ALTree/bigfloat v0.0.0-20201218142103-4a33235224ec
-	github.com/c-bata/go-prompt v0.2.5
-)
+require github.com/ALTree/bigfloat v0.0.0-20201218142103-4a33235224ec
 
-require (
-	github.com/mattn/go-colorable v0.1.7 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
-	github.com/mattn/go-runewidth v0.0.9 // indirect
-	github.com/mattn/go-tty v0.0.3 // indirect
-	github.com/pkg/term v1.1.0 // indirect
-	github.com/pointlander/peg v1.0.2-0.20260519004245-54a358d9181c // indirect
-	golang.org/x/sys v0.0.0-20200918174421-af09f7315aff // indirect
-)
+require github.com/pointlander/peg v1.0.2-0.20260519004245-54a358d9181c // indirect
 
 tool github.com/pointlander/peg
